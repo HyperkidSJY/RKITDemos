6eb8f8139dd1a0d8b84e1980ca836b015ed0f352
HyperkidSJY <shivamyadav9735@gmail.com>
2025-01-06 17:12:48 +0530

    Base Class Lib done

---
+++ DemoLibrary/PersonProcessor.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoLibrary
+{
+    public class PersonProcessor
+    {
+        public static string JoinName(string fname, string lname)
+        {
+            return fname + " " + lname;
+        }
+    }
+}
