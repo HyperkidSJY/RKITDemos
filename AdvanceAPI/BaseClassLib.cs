848349fb4c305a94510f8a5ab36ddbc0bba406d8
HyperkidSJY <shivamyadav9735@gmail.com>
2025-01-06 18:24:50 +0530

    Base class library done

---
+++ AdvanceAPI/BaseClassLib.cs
@@ -0,0 +1,21 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceAPI
+{
+    public class BaseClassLib
+    {
+        static void Main()
+        {
+            // Use the custom text writer
+            CustomTextWriter customWriter = new CustomTextWriter();
+
+            // Use the Write method of the custom writer
+            customWriter.Write('A');  // Outputs: CustomWrite: A
+        }
+    }
+}
---
+++ DemoLibrary/CustomTextWriter.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoLibrary
+{
+    public class CustomTextWriter : TextWriter
+    {
+        // Override the Encoding property (must be implemented as it's abstract)
+        public override System.Text.Encoding Encoding => System.Text.Encoding.UTF8;
+
+        // Override the Write method
+        public override void Write(char value)
+        {
+            // Simple custom logic: just print the character to the console with a prefix
+            Console.Write($"CustomWrite: {value}");
+        }
+    }
+}
