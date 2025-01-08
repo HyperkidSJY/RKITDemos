d70cfd8cd03cf2cdce4c422f53401fe677b5bb3e
HyperkidSJY <shivamyadav9735@gmail.com>
2025-01-08 16:13:54 +0530

    added Rijndael

--- AdvanceAPI/SecandCryp.cs
+++ AdvanceAPI/SecandCryp.cs
@@ -21,6 +21,14 @@ namespace AdvanceAPI
             encriptData = RSA.EncryptedByRSA(data);
             Console.WriteLine($"{data} after Encrypted By RSA : {encriptData} ");
             Console.WriteLine($"{encriptData} after Decrypted By RSA : {RSA.DecryptByRSA(encriptData)}");
+
+            string generatedKey = "0123456789ABCDEF0123456789ABCDEF";//BLRijndael.GenerateKey(keySizeInBits);
+            string iv = "0123456789ABCDEF";
+
+            // Encrypt and decrypt using Rijndael
+            encriptData = Rijndael.EncryptedByRijndael(data, generatedKey, iv);
+            Console.WriteLine($"{data} after Encrypted By Rijndael : {encriptData} ");
+            Console.WriteLine($"{encriptData} after Decrypted By Rijndael : {Rijndael.DecryptByRijndael(encriptData, generatedKey, iv)}");
         }
     }
 
@@ -110,6 +118,74 @@ namespace AdvanceAPI
 
                 // Convert the decrypted bytes to a UTF-8 encoded string
                 return Encoding.UTF8.GetString(decriptBytes);
+            }
+        }
+    }
+
+    public class Rijndael
+    {
+        /// <summary>
+        /// Encrypts the input text using Rijndael algorithm.
+        /// </summary>
+        /// <param name="plainText">Text to be encrypted</param>
+        /// <param name="key">Encryption key</param>
+        /// <param name="iv">Initialization vector</param>
+        /// <returns>Encrypted text as a Base64-encoded string</returns>
+
+        public static string EncryptedByRijndael(string plainText, string key, string iv)
+        {
+            using (RijndaelManaged rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = Encoding.UTF8.GetBytes(key);
+                rijndael.Mode = CipherMode.CFB;
+                rijndael.Padding = PaddingMode.PKCS7; // Use PKCS7 padding
+                rijndael.IV = Encoding.UTF8.GetBytes(iv);
+
+                ICryptoTransform encryptor = rijndael.CreateEncryptor(rijndael.Key, rijndael.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Decrypts the input cipher text using Rijndael algorithm.
+        /// </summary>
+        /// <param name="cipherText">Cipher text as a Base64-encoded string</param>
+        /// <param name="key">Decryption key</param>
+        /// <param name="iv">Initialization vector</param>
+        /// <returns>Decrypted text</returns>
+        public static string DecryptByRijndael(string cipherText, string key, string iv)
+        {
+            using (RijndaelManaged rijndael = new RijndaelManaged())
+            {
+                rijndael.Key = Encoding.UTF8.GetBytes(key);
+                rijndael.Mode = CipherMode.CFB;
+                rijndael.Padding = PaddingMode.PKCS7; // Use PKCS7 padding
+                rijndael.IV = Encoding.UTF8.GetBytes(iv);
+
+                ICryptoTransform decryptor = rijndael.CreateDecryptor(rijndael.Key, rijndael.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
             }
         }
     }
